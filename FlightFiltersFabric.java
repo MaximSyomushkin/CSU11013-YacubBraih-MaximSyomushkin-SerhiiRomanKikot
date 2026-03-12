af41ea9a7c78c2ece8a1f3ccd483c6efdd16fb60
MaximSyomushkin <syomushm@tcd.ie>
2026-03-12 17:05:52 +0000

    Shell of classes created

---
+++ FlightFiltersFabric.java
@@ -0,0 +1,21 @@
+
+import java.util.function.Predicate;
+
+class FlightFiltersFabric
+{
+    public static Predicate<Flight> byAirport(String airportName)
+    {
+        return 
+    }
+
+    public static Predicate<Flight> byDateRange(Date start, Date end)
+    {
+        return
+    }
+
+    public static Predicate<Flight> byAirline(String airlineName)
+    {
+        return 
+    }
+
+}
---
+++ FlightsSortersFabric.java
@@ -0,0 +1,19 @@
+import java.util.Comparator;
+
+public class FlightsSortersFabric 
+{
+    public static Comparator<Flight> byDelay(Boolean asc)
+    {
+
+    }
+
+    public static Comparator<Flight> byDistance(Boolean asc)
+    {
+
+    }
+
+    public static Comparator<Flight> byDate(Boolean asc)
+    {
+        
+    }
+}
